025f78e5533e3d4627d2976b8cc4bafbc33b2843
Du Guoxin <anton.dgx@gmail.com>
2020-10-21 00:34:22 +0800

    rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ require "faker"
 5.times do
   Restaurant.create(
     name: Faker::Restaurant.name,
-    address: Faker::Address.street_name,
+    address: Faker::Address.street_address,
     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
     )
 end
